2f9154ab4c8a11f8ade13b8ce8c0db237a6cc973
osvat0rres <113087789+osvat0rres@users.noreply.github.com>
2025-03-11 22:27:23 -0700

    Add files via upload

---
+++ database&php/buyer.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="buyer.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="main">
+        <h1>Warehouse hut</h1>
+        <h3>Enter your login</h3>
+
+        <from action="">
+            <label for="first">
+                Username:
+            </label>
+            <input type="text" id="first" name="first"
+               placeholder="Enter your Unsername" required>
+
+            <label for="password">
+                Password:
+            </label>
+
+            <input type="password" id="password" name="password"
+             placeholder="Enter your password" required>
+
+             <div class="wrap">
+                <button type="submit">
+                    Submit
+                </button>
+             </div>
+        </from>
+
+        <p>Not Registered?
+            <a href="#">
+                Create an account
+            </a>
+        </p>
+    </div>
+</body>
+</html>
---
+++ database&php/home.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="stylesheet" href="home.css">
+</head>
+<body>
+    
+
+<div id="topManu">
+      <a href="">Home</a>
+      <a>Dashboard</a>
+      <a>Products</a>
+      <a href="index.php">Contact us</a> 
+  
+
+      <div class="search">
+         <form action="3">
+            <input type="text" placeholder="Seach" name="search">
+            <button>
+                <i class="fa fa-search"></i>
+            </button>
+         </form>
+       </div>
+ </div>
+
+    <div class="header">
+        <h1>Warehouse Hut</h1>
+    </div>
+
+    <div class="container">
+        <div class="box1">
+            <a href="company.php">
+                <h1>Company</h1>
+            </a>
+
+        </div>
+
+        <div class="box2">
+            <a href="buyer.php">
+                <h1>Buyer</h1>
+            </a>
+        </div>
+    </div>
+    
+</body>
+</html>

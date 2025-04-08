e027c6f1a497cc3a66b59e527a6b7055d1726268
osvat0rres <113087789+osvat0rres@users.noreply.github.com>
2025-04-07 23:44:11 -0700

    Add files via upload

---
+++ database&php(Part2)/home.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="stylesheet" href="home.css">
+
+    <style> 
+        body{
+            background-image: url("depositphotos_648417408-stock-illustration-room-warehouse-concept-large-room.jpg");
+            background-repeat: no-repeat;
+            background-attachment: fixed;
+            background-size: cover;
+        }
+    </style>
+
+</head>
+<body>
+
+
+<div id="topManu">
+      <a href="">Home</a>
+      <a>Dashboard</a>
+      <a>Products</a>
+      <a href="index.php">Contact us</a> 
+  
+
+      <div class="search">
+         <form action="3">
+            <input type="text" placeholder="Seach" name="search">
+            <button>
+                <i class="fa fa-search"></i>
+            </button>
+         </form>
+       </div>
+ </div>
+
+    <div class="header">
+        <h1>Warehouse Hut</h1>
+    </div>
+
+    <div class="container">
+        <div class="box1">
+            <a href="register.php">
+                <h1>Company</h1>
+            </a>
+
+        </div>
+
+        <div class="box2">
+            <a href="buyer.php">
+                <h1>Buyer</h1>
+            </a>
+        </div>
+    </div>
+    
+    <div class="image-container"></div>
+</body>
+</html>

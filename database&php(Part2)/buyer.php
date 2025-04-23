434586f5e06ea9aa23bcdd575dcd3ad2b4843674
osvat0rres <113087789+osvat0rres@users.noreply.github.com>
2025-04-22 22:20:22 -0700

    Add files via upload
    new, buyers page

--- database&php(Part2)/buyer.php
+++ database&php(Part2)/buyer.php
@@ -4,39 +4,36 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>BuyerPage</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="buyer.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 <body>
-  
 
-    <!-- Top Navigation Menu -->
-    <div class="topnav">
-    <a href="#home" class="active"> <b>Buyers page</b></a>
-    <div id="myLinks">
-        <a href="#news">News</a>
-        <a href="#contact">Contact</a>
-        <a href="#about">About</a>
-    </div>
-    <a href="javascript:void(0);" class="icon" onclick="myFunction()">
-        <i class="fa fa-bars"></i>
-    </a>
-    </div>
 
- 
 
- 
-    <script>
-    function myFunction() {
-    var x = document.getElementById("myLinks");
-    if (x.style.display === "block") {
-        x.style.display = "none";
-    } else {
-        x.style.display = "block";
-    }
-    }
-    </script>
-    
 
+<!-- Top manu bar -->
+
+    <label class="manu">
+        <input type="checkbox" class="menu">
+        <div class='toggle'>
+            <span class='top_line common'></span>
+            <span class='middle_line common'></span>
+            <span class='bottom_line common'></span>
+        </div>
+
+
+        <div class="slide">
+            <h1>Buyes page</h1>
+            <ul>
+                <li><a href="#"><i class="far fa-user"></i>user</a></li>
+                <li><a href="#"><i class="fas fa-tv"></i>Dahsboard</a></li>
+                <li><a href="#"><i class="fas fa-phone"></i>Contact</a></li>
+                <li><a href="#"><i class="far fa-comments"></i>Dashboard</a></li> 
+            </ul>
+        </div>
+    </label>
 
 
 
@@ -48,16 +45,41 @@
         </form>
     </div>
 
-    <!-- Boxes -->
-    <div class="boxes">
-  
-            <div class="box1">Electronic</div>
-            <div class="box2">Home</div>
-            <div class="box3">Goods</div>
-        
-       
-    </div>
 
+    <!-- images -->
+
+    
+        <div class="column">
+            <div class="card">
+                <img src="iphone.jpg" alt='Electronicts' style='width:40%'>
+                <h2>Electronics</h2>
+                <p class='tittle'>Brand names</p>
+                <p><button class='button'>Select</button></p>
+            </div>
+        </div>
+   
+
+    
+        <div class="column">
+            <div class="card">
+                <img src="goods.webp" alt='Electronicts' style='width:40%'>
+                <h2>goods</h2>
+                <p class='tittle'>Brand names</p>
+                <p><button class='button'>Select</button></p>
+            </div>
+        </div>
+
+
+    
+        <div class="column">
+            <div class="card">
+                <img src="home.jpg" alt='Electronicts' style='width:40%'>
+                <h2>Electronics</h2>
+                <p class='tittle'>house</p>
+                <p><button class='button'>Select</button></p>
+            </div>
+        </div>
+   
 
 
 

912e28dfdbe0248b3d8896c6d7f1fcc475dfed03
Lakio12 <paulungos93@gmail.com>
2025-04-30 18:21:57 -0700

    Hot Fix 4/30/25
    I think I found the issue with this one. The search_product logic was on the top of this php file but there is already a link to it embedded in the buyer.php file. A case of double dip

--- Website(P)/buyer.php
+++ Website(P)/buyer.php
@@ -31,9 +31,7 @@
             </ul>
         </div>
     </label>
-
-
-
+	
     <!-- Search button -->
     <div class="container">
         <form>
@@ -52,47 +50,9 @@
                 <h2>Electronics</h2>
                 <p class='title'>Brand names</p>
                 <p><button class='button'>Select</button></p>
-
-
-<?php
-require '../db.php';  // Ensure this file returns a PDO connection object
-
-// Enable error reporting for debugging
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-
-// Set the content type to JSON
-header('Content-Type: application/json');
-
-// Check if the 'query' parameter is set
-if (isset($_GET['query'])) {
-    // Prepare the SQL statement using PDO
-    $searchQuery = '%'. $_GET['query']. '%';
-    $stmt = $conn->prepare("SELECT * FROM products WHERE productName LIKE :query");
-    $stmt->bindParam(':query', $searchQuery, PDO::PARAM_STR);
-
-    // Execute the statement
-    $stmt->execute();
-
-    // Fetch all results as an associative array
-    $searchResults = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-    // Return results as JSON
-    echo json_encode($searchResults);
-} else {
-    // Return an error message if 'query' parameter is not set
-    echo json_encode(['error' => 'No search query provided']);
-}
-
-// Close the database connection
-$conn = null;
-?>
-
             </div>
         </div>
    
-
-    
         <div class="column">
             <div class="card">
                 <img src="images/goods.webp" alt='Electronics' style='width:40%'>
@@ -102,8 +62,6 @@ $conn = null;
             </div>
         </div>
 
-
-    
         <div class="column">
             <div class="card">
                 <img src="images/home.jpg" alt='Electronics' style='width:40%'>
